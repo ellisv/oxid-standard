bb1e462316e6da1d2275cf0ec7e0c5bdd97eb4f3
Eligijus Vitkauskas <eligijusvitkauskas@gmail.com>
2015-01-24 22:53:33 +0200

    Structure app directory

---
+++ app/AppKernel.php
@@ -0,0 +1,20 @@
+<?php
+
+use Symfony\Component\HttpKernel\Kernel;
+use Symfony\Component\Config\Loader\LoaderInterface;
+
+class AppKernel extends Kernel
+{
+    public function registerBundles()
+    {
+        return array(
+            new Ellis\Oxid\Bundle\FrameworkBundle\FrameworkBundle(),
+            new Symfony\Bundle\MonologBundle\MonologBundle(),
+        );
+    }
+
+    public function registerContainerConfiguration(LoaderInterface $loader)
+    {
+        $loader->load(__DIR__.'/config/config_'.$this->getEnvironment().'.yml');
+    }
+}
